845d1c81281e111e6bfed76e83051839e30b1ae9
k21rs015 <113566831+k21rs015@users.noreply.github.com>
2025-05-19 12:07:53 +0900

    コマンドライン引数でファイルでみるやつ

--- src/rcat.rb
+++ src/rcat.rb
@@ -1,11 +1,12 @@
 #
-# reader.rbの各行を1行づつ画面に表示
-file=File.open"reader.rb","r"
-
-while line = file.gets&.chomp
-  puts line
+#コマンドラインで指定したすべてのファイルの各行を1行づつ
+#ruby rcat.rb a.txt b.txt c.rb
+#
+ARGV.each do |arg|
+  File.open arg,"r" do |file|
+    while line = file.gets&.chomp
+       puts line
+     end
+   end  
 end
 
-file.close
-
-#File.open "reader.rb"
