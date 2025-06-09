492094477c48f97efcff7f778e314632288dcce1
k21rs015 <113566831+k21rs015@users.noreply.github.com>
2025-06-09 11:52:02 +0900

    ヘッダ情報を表示しないように改良
    ヘッダでなくなったよ

--- src/client1.rb
+++ src/client1.rb
@@ -1,17 +1,25 @@
 #! /usr/local/bin/ruby
 
 #./client1.rb ホスト名　パス名
-#
+#ヘッダ情報を表示しないように改良
+
 require "socket"
 
-#host = ARGV[0]
-#path = ARGV[1]
 host, path = ARGV
 
 s = TCPSocket.open(host, "http")
 s.print("GET #{path} HTTP/1.1\r\n")
 s.print("Host: #{host}\r\n")
+s.print("Connection: close\r\n")
 s.print("\r\n")
+
+while line=s.gets
+  break if line == "\r\n"
+end
+
+#while line=s.gets&.chomp
+# break if line.wmpty?
+#end
 
 while line=s.gets&.chomp
   puts line
