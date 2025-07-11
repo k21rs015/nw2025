4bd4cf7780f820c3f973b213d26ba0c1c8ca161d
k21rs015 <113566831+k21rs015@users.noreply.github.com>
2025-07-12 01:52:36 +0900

    レポート前のプログラムをまとめてあげておく

--- src/httpserver1.rb
+++ src/httpserver1.rb
@@ -1,23 +1,39 @@
 #! /usr/bin/ruby
+
 require "socket"
 
-def server client
-  loop do
-    b = client.gets&.chomp
-    pp b
-    client.puts(b)
-    if b.empty?()
-      client.close()
-      sleep 60
-    end
-  end
-end
-
-a = TCPServer.open(80)
-client = a.accept()
+s0 = TCPServer.open(80)
 loop do
+  sock =s0.accept
   Thread.new do
-    server(client)
+    line = sock.gets&.chomp
+    _cmd,path,_ver =line.split
+    case path
+    when "/93"
+      sock.puts "HTTP/1.0 301 Move Permanently"
+      sock.puts "Location: http://www.kyusan-u.ac.jp/"
+      sock.puts 
+    when "/hello"
+      sock.puts "HTTP/1.0 200 OK"
+      sock.puts 
+      sock.puts "Hellow, world!"
+    else 
+      path="./#{path}"
+      if File.exist? path
+        sock.puts "HTTP/1.0 200 OK"
+        sock.puts "Content-Type: text/plain; charset=UTF-8"
+        sock.puts 
+        File.open path, "r" do |file|
+          while fl = file.gets&.chomp
+            sock.puts fl
+          end
+        end
+      else
+        sock.puts "HTTP/1.0 404 Not Found"
+        sock.puts
+      end
+      sock.puts path
+    end
+    sock.close
   end
-  client = a.accept()
 end
