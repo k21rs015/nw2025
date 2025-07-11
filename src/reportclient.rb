cf7a68482aaf58738325ce78cabd69282e5af978
k21rs015 <113566831+k21rs015@users.noreply.github.com>
2025-07-12 02:00:36 +0900

    とりあえずレポートを書く前のコピペ

---
+++ src/reportclient.rb
@@ -0,0 +1,33 @@
+#! /usr/local/bin/ruby
+
+require "socket"
+
+#host, path = ARGV
+url=ARGV[0]
+url=url[7..]
+index = url.index("/")
+host = url[0,index]
+path = url[index...]
+
+puts "host:#{host}"
+puts "path:#{path}"
+
+puts url
+
+s = TCPSocket.open(host, "http")
+s.print("GET #{path} HTTP/1.1\r\n")
+s.print("Host: #{host}\r\n")
+s.print("Connection: close\r\n")
+s.print("\r\n")
+
+while line=s.gets
+  break if line == "\r\n"
+end
+
+#while line=s.gets&.chomp
+# break if line.wmpty?
+#end
+
+while line=s.gets&.chomp
+  puts line
+end
---
+++ src/reportserver.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+require "socket"
+def server sock
+    while buf = sock.gets
+        p buf
+        sock.puts "reply:#{buf}"
+    end
+    sock.close 
+end
+
+#ポート番号80番でopen
+s0=TCPServer.open(80)
+while  true
+  sock=s0.accept
+  Thread.new do
+    server sock
+  end 
+end
+s0.close
